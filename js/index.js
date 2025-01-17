65d41204183022b8fd5cd76654527125dd9c1db8
ritikavarghese <728423@sylvaniastudents.org>
2025-01-17 00:58:41 -0500

    js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const tourDates = document.querySelectorAll('.tour-date'); // Target individual tour date items
+  const tourDates = document.querySelectorAll('.tour-date'); 
 
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        // Add the "visible" class when the section is in the viewport
         entry.target.classList.add('visible');
       }
     });
-  }, { threshold: 0.5 }); // Trigger when 50% of the section is in the viewport
-
-  // Observe each tour date item
+  }, { threshold: 0.5 }); 
   tourDates.forEach(tourDate => observer.observe(tourDate));
 });
